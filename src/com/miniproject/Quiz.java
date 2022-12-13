fd1d839033894337805d36cb4384fbdbfb45c940
unknown <ankitabhagat715@gmail.com>
2022-12-13 17:29:22 +0530

    Added suceesfuully

--- src/com/miniproject/Quiz.java
+++ src/com/miniproject/Quiz.java
@@ -1,6 +1,7 @@
 package com.miniproject;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -15,21 +16,21 @@ public class Quiz {
 	int wrongAns = 0;
 
 	// b,a,d,c,b,c,a,b,c,c
-	public  void getQuizTest(String name) {
-		Question q1 = new Question("1.Who is founder of java?", "a.Van Rossum", "b.James Gosling", "c.Dennis Ritchie",
+	public  void getQuizTest(int id) {
+		Question q1 = new Question("'Who is founder of java?", "a.Van Rossum", "b.James Gosling", "c.Dennis Ritchie",
 				"d.Bjarne Stroustrup");
-		Question q2 = new Question("2.When java was invented?", "a.1995", "b.1996", "c.1918", "d.1999");
-		Question q3 = new Question("3.How many pillars of Oops concept?", "a.3", "b.2", "c.5", "d.4");
-		Question q4 = new Question("4.How many methods in object class?", "a.13", "b.12", "c.11", "d.15");
-		Question q5 = new Question("5.String is the class that represents sequence of……", "a.Object", "b.Character",
+		Question q2 = new Question("When java was invented?", "a.1995", "b.1996", "c.1918", "d.1999");
+		Question q3 = new Question("How many pillars of Oops concept?", "a.3", "b.2", "c.5", "d.4");
+		Question q4 = new Question("How many methods in object class?", "a.13", "b.12", "c.11", "d.15");
+		Question q5 = new Question("String is the class that represents sequence of……", "a.Object", "b.Character",
 				"c.Class", "d.Array");
-		Question q6 = new Question("6.Number of primitive data types in java are?", "a.6", "b.7", "c.8", "d.9");
-		Question q7 = new Question("7.What is size of float and double in java?", "a.32 & 64", "b.32 & 32", "c.64 & 64",
+		Question q6 = new Question("Number of primitive data types in java are?", "a.6", "b.7", "c.8", "d.9");
+		Question q7 = new Question("What is size of float and double in java?", "a.32 & 64", "b.32 & 32", "c.64 & 64",
 				"d.64 & 32");
-		Question q8 = new Question("8.Array in java are…", "a.Object reference", "b.Objects", "c.Primitive data type",
+		Question q8 = new Question("Array in java are…", "a.Object reference", "b.Objects", "c.Primitive data type",
 				"d.None");
-		Question q9 = new Question("9.CompareTo() returns", "a.True", "b.False", "c.An int value", "d.None");
-		Question q10 = new Question("10.Total constructor string class have?", "a.3", "b.7", "c.13", "d.20");
+		Question q9 = new Question("CompareTo() returns", "a.True", "b.False", "c.An int value", "d.None");
+		Question q10 = new Question("Total constructor string class have?", "a.3", "b.7", "c.13", "d.20");
 
 		HashMap<Question, Character> hm = new HashMap<>();
 		hm.put(q1, 'b');
@@ -43,14 +44,16 @@ public class Quiz {
 		hm.put(q9, 'c');
 		hm.put(q10, 'c');
 
+		int q=1;
 		
 
 		for (Map.Entry<Question, Character> map : hm.entrySet()) {
-			System.out.println(map.getKey().getQuestion());
+			System.out.println(q+") "+map.getKey().getQuestion());
 			System.out.println(map.getKey().getOpt_1());
 			System.out.println(map.getKey().getOpt_2());
 			System.out.println(map.getKey().getOpt_3());
 			System.out.println(map.getKey().getOpt_4());
+			q++;
 
 			Scanner sc = new Scanner(System.in);
 			System.out.println("Enter Your Answer: ");
@@ -61,12 +64,10 @@ public class Quiz {
 
 				correctAns++;
 
-			} else {
-				wrongAns++;
-			}
+			} 
 		}
 		System.out.println();
-		System.out.println("***************Result****************");
+		System.out.println("\t\t\t\t***************Result****************\n");
 		System.out.println("Total Questions: " + hm.size());
 		System.out.println("Correct Answered Questions : " + correctAns);
 
@@ -74,40 +75,57 @@ public class Quiz {
 
 		if (Score > 8) {
 			System.out.println("Garde : A");
-			setStudentDetails(name, Score, "A");
+			setStudentDetails( id,Score, "A");
 		} else if (Score >= 5 && Score <= 8) {
 			System.out.println("Grade : B");
-			setStudentDetails(name, Score, "B");
+			setStudentDetails( id,Score, "B");
 		} else if (Score == 5) {
 			System.out.println("Grade : C");
-			setStudentDetails(name, Score, "C");
+			setStudentDetails( id,Score, "C");
 		} else {
-			System.out.println("Fail");
-			setStudentDetails(name, Score, "Fail");
+			System.out.println(" Student Result--Fail");
+			setStudentDetails( id,Score, "Fail");
 
 		}
 
 	}
 
-	public void setStudentDetails(String name, int score, String grade) {
+	public void setStudentDetails(int id, int score, String grade) {
 		ConnectionStud cs = new ConnectionStud();
 		try {
 			Connection con = cs.getConnectionDetails();
 			PreparedStatement ps = con
-					.prepareStatement("insert studentquiz (stud_name,stud_score,stud_grade) values (?,?,?)");
-			ps.setString(1, name);
-			ps.setInt(2, score);
-			ps.setString(3, grade);
+					.prepareStatement("update studentquiz set stud_score=?,stud_grade=? where stud_id=?");
 			
-			PreparedStatement ps1=con.prepareStatement("Update studentquiz stud_name=? where stud_id=?");
+			ps.setInt(1, score);
+			ps.setString(2, grade);
+			ps.setInt(3,id);
+			
+			//PreparedStatement ps1=con.prepareStatement("");
 			int rs = ps.executeUpdate();
+			
+
+	
+		
 	}
 	
 		catch(Exception e) {
 			
 		}
+		
 	}
-}
+	/*public static void main(String[] args) {
+		Quiz q=new Quiz();
+		q.getQuizTest(1);*/
+		
+	}
+	
+
+	
+
+		
+	
+	
 
 
 
